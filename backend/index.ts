722dc92130a9e180118f561592c94afa07d0fc84
ignacio <ignaciofranciscovigo@gmail.com>
2025-02-01 21:50:30 -0300

    Initial commit.

---
+++ backend/index.ts
@@ -0,0 +1,21 @@
+import express from "express";
+const app = express();
+
+/* MIDDLEWARE */
+
+app.use(express.json());
+
+/* ROUTES */
+
+app.get("/api/ping", (_req, res) => {
+  console.log("ping reached.");
+  res.send("pong");
+});
+
+/* PORT */
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
